0f542a1e815a58b25e5a18ce06b5d3d51000e745
leroykayanda <leroykayanda@gmail.com>
2019-04-03 20:35:25 +0300

    b2c

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -14,6 +14,11 @@ export class AccountPage {
 user: any = {};
 pass1;
 pass2;
+balance;
+swiftpay_fee=0;
+mpesa_fees=0;
+towithdraw=0;
+phone="07";
 
 constructor(
     public navCtrl: NavController, 
@@ -28,11 +33,84 @@ constructor(
 ionViewDidLoad() {
     if(localStorage.getItem('user')){
         this.user=JSON.parse(localStorage.getItem('user'));  
+        console.log(this.user);
     }
+
+    this.getBalance();
 }
 
 openMenu() {
     this.menuCtrl.open();
+}
+
+withdraw()
+{
+    if(this.balance<100)
+    {
+        let toast = this.toastCtrl.create({
+            message: "Minimum amount is shs. 100",
+            showCloseButton: true,
+            position: 'bottom'
+        });
+
+        toast.present();
+    }else
+    {
+        this.prov.show_loader('Please wait ..');
+        var link=this.prov.php+'b2c.php';
+        var myData = JSON.stringify(
+            {
+                amount:this.towithdraw,
+                phone:this.phone,
+                uid:this.user.id
+            });
+
+        this.http.post(link, myData)
+            .subscribe(data => {
+
+            this.prov.dismiss_loader();
+            this.getBalance();
+            
+            let res= data;
+            console.log(res);
+
+            let toast = this.toastCtrl.create({
+                message: res['msg'],
+                showCloseButton: true,
+                position: 'bottom'
+            });
+
+            toast.present();
+
+        }, error => {
+            console.log(error);
+        });
+    }
+}
+
+getBalance()
+{
+    var myData = JSON.stringify(
+        {
+            uid: this.user.id
+        }
+    );
+    var link=this.prov.php+'getBalance.php';
+
+    this.http.post(link, myData)
+        .subscribe(r => {
+        let res=r;
+
+        console.log(res);
+        this.balance=res['balance'];
+        this.phone=res['phone'];
+        this.swiftpay_fee=Math.round(this.balance*0.14);
+        this.mpesa_fees=(this.balance < 1000 ? 15 : 22 );
+        this.towithdraw=this.balance-this.swiftpay_fee-this.mpesa_fees;
+
+    }, error => {
+        console.log(error);
+    });
 }
 
 openPage(page){ 
